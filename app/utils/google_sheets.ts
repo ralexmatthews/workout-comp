eb05c8226a31bd5a565bc8200420eddb55aded4e
Alex Matthews <amatthews4851@gmail.com>
2024-09-25 22:08:45 -0500

    Updates a row if the date already exists

--- app/utils/google_sheets.ts
+++ app/utils/google_sheets.ts
@@ -1,9 +1,12 @@
+import { JWT } from "google-auth-library";
 import {
   GoogleSpreadsheet,
   type GoogleSpreadsheetRow,
 } from "google-spreadsheet";
-import { JWT } from "google-auth-library";
 
+/**
+ * Get the google doc with some validation that we have the appropriate ENV vars
+ */
 const getGoogleSheet = async () => {
   const {
     GOOGLE_SERVICE_ACCOUNT_EMAIL,
@@ -45,22 +48,52 @@ type RowData = {
   steps: number;
 };
 
+/**
+ * Add a row to the google doc. Takes a day's workout data and saves it. If the
+ * date already exists, it updates that row instead.
+ *
+ * @param row The values to add to the spreadsheet row
+ */
 export const addRow = async (row: RowData) => {
   const doc = await getGoogleSheet();
 
   const sheet = doc.sheetsByTitle[row.person];
-  await sheet.addRow({
-    date: row.date,
-    move: row.movePoints,
-    stand: row.standPoints,
-    exercise: row.exercisePoints,
-    bonus: row.bonusPoints,
-    steps: row.steps,
-  });
+
+  const data = await sheet.getRows();
+
+  // check if the date already exists
+  const rowThatAlreadyExists = data.find(
+    (datum) => datum.get("date") === row.date
+  );
+
+  // if it does, just update that row instead
+  if (rowThatAlreadyExists) {
+    rowThatAlreadyExists.assign({
+      move: row.movePoints,
+      stand: row.standPoints,
+      exercise: row.exercisePoints,
+      bonus: row.bonusPoints,
+      steps: row.steps,
+    });
+    await rowThatAlreadyExists.save();
+  } else {
+    // otherwise add a new row with that data
+    await sheet.addRow({
+      date: row.date,
+      move: row.movePoints,
+      stand: row.standPoints,
+      exercise: row.exercisePoints,
+      bonus: row.bonusPoints,
+      steps: row.steps,
+    });
+  }
 };
 
 export type SheetDatum = Omit<RowData, "person">;
 
+/**
+ * Converts the google sheet row to a usable js object in the form we expect
+ */
 const normalizeRow = (
   row: GoogleSpreadsheetRow<Record<string, unknown>>
 ): SheetDatum => ({
@@ -72,6 +105,9 @@ const normalizeRow = (
   steps: Number(row.get("steps")),
 });
 
+/**
+ * Gets all the data in our spreadsheet, for all people
+ */
 export const getAllData = async () => {
   const doc = await getGoogleSheet();
 
